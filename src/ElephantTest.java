6993aafefb6a35539ddcdfcd24e24eac174348bc
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-06-07 17:49:27 +0700

    Add test for elephant and lion

---
+++ src/ElephantTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ElephantTest {
+    private final Elephant elephant = new Elephant(Color.BLUE, 0, 1);
+    private final Lion lion = new Lion(Color.RED, 0, 2);
+    @org.junit.jupiter.api.Test
+    void canKill() {
+        assertEquals(true, elephant.canKill(lion));
+    }
+}
---
+++ src/LionTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LionTest {
+    private final Lion lion = new Lion(Color.RED, 0, 1);
+    private final Elephant elephant = new Elephant(Color.BLUE, 0, 2);
+    @Test
+    void canKill() {
+        assertEquals(false, lion.canKill(elephant));
+    }
+}
