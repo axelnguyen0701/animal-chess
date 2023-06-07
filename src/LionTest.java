d24a2ba62a5218d0208e112f3d99f5f209bffff1
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-06-07 18:09:50 +0700

    Add test see if lion can kill tiger

--- src/LionTest.java
+++ src/LionTest.java
@@ -5,8 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class LionTest {
     private final Lion lion = new Lion(Color.RED, 0, 1);
     private final Elephant elephant = new Elephant(Color.BLUE, 0, 2);
+    private final Tiger tiger = new Tiger(Color.BLUE, 0, 2);
     @Test
     void canKill() {
         assertEquals(false, lion.canKill(elephant));
+        assertEquals(true, lion.canKill(tiger));
     }
 }
