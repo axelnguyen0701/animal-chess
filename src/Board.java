cc0bdc3fe0e1366316fdf4a5f7796cf5684eb4a3
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-06-16 15:18:01 +0700

    add board

---
+++ src/Board.java
@@ -0,0 +1,12 @@
+import pieces.Color;
+import pieces.Tiger;
+
+public class Board {
+    Spot[][] board;
+    public Board() {
+        initBoard();
+    }
+
+    private void initBoard() {
+    }
+}
