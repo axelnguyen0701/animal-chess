8dcacc07a3864a8a22024ccf10176f43253b6996
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-06-21 16:51:16 +0700

    Add setter and getter for move

--- src/Move.java
+++ src/Move.java
@@ -15,4 +15,43 @@ public class Move {
         this.pieceMoved = start.getPiece();
     }
 
+    public Spot getStart() {
+        return start;
+    }
+
+    public void setStart(Spot start) {
+        this.start = start;
+    }
+
+    public Spot getEnd() {
+        return end;
+    }
+
+    public void setEnd(Spot end) {
+        this.end = end;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Piece getPieceMoved() {
+        return pieceMoved;
+    }
+
+    public void setPieceMoved(Piece pieceMoved) {
+        this.pieceMoved = pieceMoved;
+    }
+
+    public Piece getPieceKilled() {
+        return pieceKilled;
+    }
+
+    public void setPieceKilled(Piece pieceKilled) {
+        this.pieceKilled = pieceKilled;
+    }
 }
